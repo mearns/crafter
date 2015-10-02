6d2367fa905c84608b359ca2b4770281a0ff713f
Brian Mearns <bmearns@ieee.org>
2015-10-02 21:13:36 +0100

    More unit testing for ListBuilder.

--- src/test/java/com/brianmearns/crafter/ListBuilderTest.java
+++ src/test/java/com/brianmearns/crafter/ListBuilderTest.java
@@ -23,13 +23,13 @@ public class ListBuilderTest {
     }
 
     @Test
-    public void testAddAll_Iterable_T_withArray() throws Exception {
+    public void testAddAll_T_array() throws Exception {
         final String[] expected = new String[]{"burley", "hurley", "Fran"};
         ListBuilder<String> uut = ListBuilder.create();
         ListBuilder<String> res = uut.addAll(expected);
 
         assertSame("Expect value returned by add(T) is the original builder.", uut, res);
-        assertArrayEquals("Expected addAll to add the given array to the list.", expected, uut.get().toArray());
+        assertArrayEquals("Expected addAll(T[]) to add the given array to the list.", expected, uut.get().toArray());
     }
 
     @Test
@@ -39,6 +39,17 @@ public class ListBuilderTest {
         ListBuilder<String> res = uut.addAll(expected);
 
         assertSame("Expect value returned by add(T) is the original builder.", uut, res);
-        assertArrayEquals("Expected addAll to add the given list to the list.", expected.toArray(), uut.get().toArray());
+        assertArrayEquals("Expected addAll(Collection<T>) to add the given list to the list.", expected.toArray(), uut.get().toArray());
     }
+
+    @Test
+    public void testAddAll_Iterator_T() throws Exception {
+        final List<String> expected = Arrays.asList("burley", "hurley", "Fran");
+        ListBuilder<String> uut = ListBuilder.create();
+        ListBuilder<String> res = uut.addAll(expected.iterator());
+
+        assertSame("Expect value returned by add(T) is the original builder.", uut, res);
+        assertArrayEquals("Expected addAll(Iterator<T>) to add the given items to the list.", expected.toArray(), uut.get().toArray());
+    }
+
 }
