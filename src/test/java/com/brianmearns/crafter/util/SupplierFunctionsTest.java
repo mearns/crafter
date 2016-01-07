8861aef059201663857d87fe373ce4fd36d84df8
Brian Mearns <bmearns@ieee.org>
2016-01-07 09:37:20 -0500

    Some comments.

--- src/test/java/com/brianmearns/crafter/util/SupplierFunctionsTest.java
+++ src/test/java/com/brianmearns/crafter/util/SupplierFunctionsTest.java
@@ -11,13 +11,13 @@ public class SupplierFunctionsTest {
 
     @Test
     public void testSupplierOfInstanceFunction() throws Exception {
-        //We already tested the function, we just need to make sure they're actually returned by this factory function.
+        //We already tested the function in SupplierOfInstanceFunctionTest, we just need to make sure they're actually returned by this factory function.
         assertNotNull("Method should return a Function, not null.", SupplierFunctions.supplierOfInstanceFunction());
     }
 
     @Test
     public void testBuilderToSupplierFunction() throws Exception {
-        //We already tested the function, we just need to make sure they're actually returned by this factory function.
+        //We already tested the function in BuilderToSupplierFunctionTest, we just need to make sure they're actually returned by this factory function.
         assertNotNull("Method should return a Function, not null.", SupplierFunctions.builderToSupplierFunction());
     }
 }
